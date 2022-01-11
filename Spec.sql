d0e95191356edeca7f092b08c3a2f9ceaf9bceec
SuriRose <surie680@gmail.com>
2022-01-11 16:31:05 -0500

    Shoe  Store Spec

---
+++ Spec.sql
@@ -0,0 +1,53 @@
+/* Hi, I'm the owner of a shoe store, called LA CHAUSSURE. We need an organized system to keep track of everything.
+We heard such great things about your work, we know that you are the right person to help us out.
+
+We need a record of style number, shoe style, size, color and company.
+Every style number is different.
+We need to know when the record was entered into the system, a date when shoes were purchased, how much they cost, when they were sold and for how much.
+Another important detail, did the customer buy on sale or regular price.
+Our return policy is same day money back and 30 day exchange, besides for sale items which can't be returned.
+We also would like to know which employee served the customer so they can earn commission.
+Another thing every shoe needs is a style number combined of first three letters of style, a dash, the last two letters of company and the first two letters of the color in uppercase.
+
+Questions:
+Q.Do you ever allow a customer to exchange a shoe after  30 days or return the next day ?
+A.Sometimes customers try to return after the day they bought it, but it's against our policy.
+
+Q. Do you ever donate shoes for free ?
+A.Yes, sometimes, when the shoe didn't sell and is out of style.
+
+Q.What is your most expensive pair of shoes that you sold ?
+A.Our most expensive pair of shoes is 450.99, we never sell for a higher price than that.
+
+Q.Are you particular about the timing of when the shoe was bought, in terms of returning or exchanging?
+A.No, as long as it's within those 24 hours it's fine.
+
+Reports:
+1. We would like a list of all the inventory that sold within a month of purchase, in order of size and style.
+2. We would like to give a bonus to the salesperson that brings in the most sales, show which salesperson that is.
+3. Which style, sold the most, so we know what to reorder more of.
+4. Which shoes got returned the most.
+5. The amount of profit we earn on each pair of shoes.
+6. The average amount of days it takes for inventory to be sold.
+7. Which month people buy the most.
+
+Sample Data:
+Style, Size, Color, PurchasePrice, PurchaseDate, SalePrice, SaleDate, Returned, Sale, Salesperson, ShoeCompany
+            
+loafer,  34, green, 94.99, Aug.30.2021, 100.99, sep.1.2021, null, null, Mr.Green, Venittini
+step in, 29, brown, 100.99, Aug.25.2021, 110.99, sep.8.2021, null, null, Ms.Weiss, Venittini
+high top, 20, rust, 79.99, Sep.1.2021, 89.99, June.23.2021, returned, null, Mr.Shwartz, Jacadi
+high top, 23, pink,  79.99, Sep.1.2021, 89.99, June.23.2021, returned, null, Mr.Shwartz, Jacadi 
+high top, 25, green,  79.99, Sep.1.2021, 89.99, June.23.2021, returned, null, Mr.Shwartz, Jacadi  
+loafer,   5, tan, 120.99, Aug.1.2021, 130.99, Sep.4.2021, null, Mr.Green, null, Michael Kors
+High heel, 7, black velvet, 100.00, sep.3.2019, 105.00, null, Mrs.Klein, sale, Vince 
+loafer,  34, black, 95.00, Aug.30.2021, 100.00, Jan.2.2021, null, null, Mrs.Klein, Pardoo
+Bootie,  33, black, 90.99, Aug.30.2021, 100.00, Jan.1.2020, null, sale, Mr.Shwartz, Steve Madden
+loafer,  32, black, 90.99, Aug.30.2021, 100.00, Sep.2.2020, returned, sale, Ms.Weiss, Pardoo
+loafer,  22, black, 90.99, Aug.30.2021, 100.00, Oct.5.2020, returned, sale, Ms.Weiss, Pardoo
+lace up,  35, maroon, 100.00, Aug.30.2021, 150.99, Sep.7.2021, null,null, Mrs.Klein, Lillie
+lace up,  35, maroon, 100.00, Aug.30.2021, 150.99, Oct.7.2021, null,null, Mr.Green, Lillie
+Ballet flat, 8, luggage seude, 150.00, Aug.30.99, 175.99, null, null, null, null, Sam Edelman 
+
+Thank you!/*
+
